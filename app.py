c964a560a3e52773fcf47b92620faa11b4968b2e
Archit Prasad <architprasad24@gmail.com>
2025-11-25 22:26:42 +0530

    Fix RetrievalQA import error

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 
 # Load environment variables
 load_dotenv()
